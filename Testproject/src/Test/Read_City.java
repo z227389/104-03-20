a709332b54224935abe96cd071394d11c92402d0
user <user@X550VC>
2015-04-10 03:31:34 +0800

    TEST

--- Testproject/src/Test/Read_City.java
+++ Testproject/src/Test/Read_City.java
@@ -1,29 +1,35 @@
 package Test;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 public class Read_City {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 	 	  String[] s2;
-	 	  String s3;  
-	    	City[] cities = new City[52];
+	 	  String s3;
+	 	  
+	    	//City[] cities = new City[52];
+	 	 ArrayList<Integer> cities = new ArrayList<Integer>();
 	         FileReader fr2=new FileReader("berlin52.txt");
 	       	BufferedReader sr2=new BufferedReader(fr2);
-	          for ( int i = 0; i < cities.length; i++){
+	       	
+	    
+	          for ( int i = 0; i < cities.size(); i++){
+	        	  
 	            s3 = sr2.readLine();
 	            s2 = s3.split("\\s+");
-	            cities[i] = new City();
-	            cities[i].setX((int) (Double.parseDouble(s2[1])));
-	            cities[i].setY((int) (Double.parseDouble(s2[2])));
-	            System.out.println(cities[i].getX());
-
+	            
+	            cities.add((int)(Double.parseDouble(s2[1])));
+	         
+	            System.out.println(cities.get(i));
 	          			}
 	          
 	          }
 }
 	   
  
+
